075774f5fc8a6fd4d3f243a116e8ca1cda5b3e6e
Fatlinad0 <fb55487@ubt-uni.net>
2023-03-11 18:05:36 +0100

    Fatlinda

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -37,6 +37,8 @@ const quick__links2 = [
 ]
 
 const Footer = () => {
+
+  const year = new Date().getFullYear()
   return (
     <Container>
       <Row>
@@ -48,22 +50,22 @@ const Footer = () => {
               <div className="social__links d-flex align-items-center gap-4">
                 <span>
                   <Link to='#'>
-                    <i class="ri-youtube-line"></i>
+                    <i class="ri-youtube-fill"></i>
                   </Link>
                 </span>
                 <span>
                   <Link to='#'>
-                    <i class="ri-instagram-line"></i>
+                    <i class="ri-instagram-fill"></i>
                   </Link>
                 </span>
                 <span>
                   <Link to='#'>
-                    <i class="ri-facebook-line"></i>
+                    <i class="ri-facebook-fill"></i>
                   </Link>
                 </span>
                 <span>
                   <Link to='#'>
-                    <i class="ri-discord-line"></i>
+                    <i class="ri-discord-fill"></i>
                   </Link>
                 </span>
               </div>
@@ -94,7 +96,7 @@ const Footer = () => {
           <ListGroup className="footer__quick-links">
               <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
                 <h6 className="mb-0 d-flex align-items-center gap-2">
-                  <span><i class="ri-map-pin-line"></i></span>
+                  <span><i class="ri-map-pin-fill"></i></span>
                   Address:
                 </h6>
                 <p className="mb-0">Pristina, Kosovo</p>
@@ -103,7 +105,7 @@ const Footer = () => {
           <ListGroup className="footer__quick-links">
               <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
                 <h6 className="mb-0 d-flex align-items-center gap-2">
-                  <span><i class="ri-mail-line"></i></span>
+                  <span><i class="ri-mail-fill"></i></span>
                   Email:
                 </h6>
                 <p className="mb-0">girls.world@gmail.com</p>
@@ -112,12 +114,16 @@ const Footer = () => {
           <ListGroup className="footer__quick-links">
               <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
                 <h6 className="mb-0 d-flex align-items-center gap-2">
-                  <span><i class="ri-phone-line"></i></span>
+                  <span><i class="ri-phone-fill"></i></span>
                   Phone:
                 </h6>
                 <p className="mb-0">+383 49 781 854</p>
               </ListGroupItem>
           </ListGroup>
+        </Col>
+        <Col lg='12'  className="text-center pt-5">
+          <p className="copyright">© Copyright {year} by 
+          <span> Fatlinda Berisha, Edona Cadraku and Alsona Smajli</span></p>
         </Col>
       </Row>
     </Container>

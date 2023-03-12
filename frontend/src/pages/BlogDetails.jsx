16ffbfacd032ecac74220ca01029ac65075ab44b
edona12 <ec51855@ubt-uni.net>
2023-03-12 11:19:41 -0500

    Edona

--- frontend/src/pages/BlogDetails.jsx
+++ frontend/src/pages/BlogDetails.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import '../styles/blog-details.css'
-import { Container, Row, Col, Form } from 'reactstrap'
+import { Container, Row, Col, Form, ListGroup } from 'reactstrap'
 import {useParams} from 'react-router-dom'
 import blogData from '../assets/data/blogs'
 import calculateAvgRating from '../utils/avgRating'
+
 import { Booking } from '../components/Booking/Booking'
+import avatar from "../assets/images/avatar.jpg"
 
 
 const BlogDetails = () => {
   const {id}= useParams()
   const blog= blogData.find(blog=>blog.id===id)
 
-  const {photo, title, description, reviews, city, maxGroupSize}=blog
+  const {photo, title, description, reviews, city,ListGroup, maxGroupSize}=blog
 
   const {totalRating,avgRating}=calculateAvgRating(reviews)
 
@@ -48,7 +50,7 @@ const BlogDetails = () => {
               <p>{description}</p>
             </div>
             {/* ky eshte sektori per reviews */}
-          <div className="tour__reviws mt-4">
+          <div className="blog__reviws mt-4">
             <h4>Reviews({reviews?.length}reviews)</h4>
             <Form>
               <div className="d-flex align-itmes-center gap-3 mb-4 rating__group">
@@ -58,16 +60,32 @@ const BlogDetails = () => {
                4<span><i class="ri-star-s-fill"></i></span>
                5<span><i class="ri-star-s-fill"></i></span>
               </div>
+              <div className="review_input">
+                <input type="text" placeholder="share your thoughts"/>         
+                <button className='btn primary__btn text-white'
+                type='submit'>Submit </button>
+                     </div>
             </Form>
-          </div>
-          {/*ketu mbaron sektori i reviews */}
-          </div>
+            <ListGroup className="user__reviews">
+              { reviews?.map(review=>(
+                <div className='reviews__item'>
+                  <img src={avatar} alt="" />
+                  <div className="w-100">
+                    <div className='d-flex align-items-center justify-sontent-between'>
+                    </div>
+                  </div>
+                </div>
+              ))} 
+            </ListGroup>
           
-          <Col lg="4">
+        {/*ketu mbaron sektori i reviews */}
+        </div>
+  
+        <Col lg="4">
             <Booking blog={blog} />
-          </Col>
-
         </Col>
+      </div>
+      </Col>
       </Row>
     </Container>
   </section>
